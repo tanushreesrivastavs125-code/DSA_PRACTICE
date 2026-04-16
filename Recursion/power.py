00060b35869be1e46eba0ceab8338acddc50b5ae
tanushreesrivastavs125-code <tanushree.srivastav.s.125@kalvium.community>
2026-04-16 11:41:16 +0530

    time limit exceeded

---
+++ Recursion/power.py
@@ -0,0 +1,28 @@
+#  x^n = 2^3
+
+# def power_ofnum(x,n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return x * power_ofnum(x,n-1)
+
+# print(power_ofnum(2,3))
+
+# Time Limit Exceeded
+
+def power_ofnum(x,n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return x
+    if n<0:
+        n = -n
+        x = 1/x
+    half = power_ofnum(x,n//2)
+    if n%2 == 0:
+        return half*half
+    else:
+        return x*half*half
+    
+print(power_ofnum(2,3))
+print(power_ofnum(2,-3))
---
+++ Recursion/reverse_strr.py
@@ -0,0 +1,9 @@
+# tanu == rev(unat)
+
+def reverse_strr(strr):
+    if len(strr) == 0:
+        return ""
+    else:
+        return strr[-1] + reverse_strr(strr[:-1])
+    
+print(reverse_strr("tanu"))
