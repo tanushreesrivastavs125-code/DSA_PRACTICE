6eb2c8ee09c2ad5f1f8e982e8dce56562750ab5d
tanushreesrivastavs125-code <tanushree.srivastav.s.125@kalvium.community>
2026-05-06 14:46:33 +0530

    hello

--- Two pointer/valid_palindrom.py
+++ Two pointer/valid_palindrom.py
@@ -1,2 +1,19 @@
 # s = "A man, a plan, a canal: Panama"
-def isPalindrom()
+def isPalindrom(arr):
+    left , rigth = 0 , len(arr)-1
+
+    while left < rigth:
+
+        while left < rigth and not arr[left].isalnum():
+            left += 1
+        while left < rigth and not arr[rigth].isalnum():
+            rigth -= 1
+        if arr[left].lower() != arr[rigth].lower():
+            return False
+        
+        left+=1
+        rigth-=1
+
+    return True
+    
+print(isPalindrom("A man, a plan, a canal: Panama"))
